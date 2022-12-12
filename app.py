c0d2940fedd964fc3f6bb8e2deeeb2fda8549ad5
jkashmee <83988314+jkashmee@users.noreply.github.com>
2022-12-11 20:55:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ lr.fit(X_train,Y_train)
 
 user = [par,mar,gen,edu,inc,age]
 predicted_class = lr.predict([user])
-probs = lr.predict_proba([user][0])
+probs = lr.predict_proba([user])
 
 userclass=""
 if predicted_class[0] == 1:
